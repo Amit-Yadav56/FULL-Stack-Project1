8b1e373fc0863786384c71660916a2b2bc00b110
Amit-Yadav56 <139137290+Amit-Yadav56@users.noreply.github.com>
2024-03-20 19:13:42 +0530

    Google authentication completed in login and redirection is done

--- music-player-website/client/src/App.js
+++ music-player-website/client/src/App.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Home, Login } from './components'
+import { app } from "./config/firebase.config";
+import { getAuth } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+
+
 const App = () => {
+  const firebaseAuth = getAuth(app);
+  const navigate = useNavigate();
+
+  //create a state to save the authentication is true or not
+  const [auth, setAuth] = useState(false || window.localStorage.getItem("auth") === "true")
+
+  //check firebase authentication and if not authenticated redirect to login page
+  useEffect(() => {
+
+    //authentication info
+    firebaseAuth.onAuthStateChanged((userCred) => {
+      //if cred is found then redirect to homepage and print token
+      if (userCred) {
+        userCred.getIdToken().then((token) => {
+          console.log(token)
+          navigate("/")
+        })
+      }
+      //else redirect to login page
+      else {
+        setAuth(false)
+        window.localStorage.setItem("auth", "false")
+        navigate("/login")
+      }
+    })
+  }, [])
+
   return (
     <div className='w-screen h-screen bg-primary flex justify-center items-center'>
       <Routes>
         {/* Home and login */}
-        <Route path='/login' element={<Login />} />
+        <Route path='/login' element={<Login setAuth={setAuth} />} />
         <Route path='/*' element={<Home />} />
 
       </Routes>
--- music-player-website/client/src/components/Login.jsx
+++ music-player-website/client/src/components/Login.jsx
@@ -1,22 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FcGoogle } from 'react-icons/fc'
 import { app } from "../config/firebase.config";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
 
 
-const login = () => {
+function login({ setAuth }) {
     //get forebase authentication info
-    const firebaseAuth = getAuth(app)
+    const firebaseAuth = getAuth(app);
     //google authentication provider
     const provider = new GoogleAuthProvider();
-    //navigate to the homepage
+    const navigate = new useNavigate();
+
     const loginWithGoogle = async () => {
         await signInWithPopup(firebaseAuth, provider).then((userCred) => {
 
-            console.log(userCred);
+            if (userCred) {
+                setAuth(true);
+                window.localStorage.setItem("auth", "true");
+                firebaseAuth.onAuthStateChanged((userCred) => {
+
+                    //has better way to do it
+                    //has better way to do it
+
+                    if (userCred) {
+                        console.log(userCred)
+                        navigate("/")
+                    }
+                    else {
+                        setAuth(false)
+                        navigate("/login")
+                    }
+                })
+            }
         }
         )
     }
+
 
     return (
         <div className="relative w-screen h-screen">

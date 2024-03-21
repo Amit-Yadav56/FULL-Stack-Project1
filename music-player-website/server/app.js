713afb5a9d975cb7807435419bff89ddff2c0b4a
Amit-Yadav56 <139137290+Amit-Yadav56@users.noreply.github.com>
2024-03-21 22:29:45 +0530

    working on the backend
    connecting to database

--- music-player-website/server/app.js
+++ music-player-website/server/app.js
@@ -1,14 +1,24 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const mongoose = require('mongoose')
+require("dotenv/config")
+const app = express();
 // cross origin resource sharing
-const cors=require('cors');
+const cors = require('cors');
 
 app.use(cors());
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     return res.json("Hey there");
 })
 
 
-app.listen(4000,()=>{
+mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true })
+mongoose.connection.once("open", () => {
+    console.log("Connected")
+}).on("error", (error) => {
+    console.log(`Error: ${error}`)
+})
+
+
+app.listen(4000, () => {
     console.log("listening to port 4000")
 })

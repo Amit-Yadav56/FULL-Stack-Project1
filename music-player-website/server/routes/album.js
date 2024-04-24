5b4a8e29b05db2f998cc1c71ebf00d5a65d5ca82
Amit-Yadav56 <139137290+Amit-Yadav56@users.noreply.github.com>
2024-04-24 19:48:51 +0530

    Backend partially completed as we added routes for songs and albums

--- music-player-website/server/routes/album.js
+++ music-player-website/server/routes/album.js
@@ -1,7 +1,87 @@
 const router = require("express").Router()
+const album = require("../models/albums")
 
-router.get("/getAll", async (req, res) => {
-  return res.json("getting all albums")
+//to add a new album
+router.post("/saveNew/", async (req, res) => {
+  const newAlbum = album(
+    {
+      name: req.body.name,
+      imageUrl: req.body.imageUrl,
+    }
+  );
+
+  try {
+    //save is a method comming from mongoose provided by specific model i.e(album)
+    const saveAlbum = await newAlbum.save();
+    return res.status(200).send({ sucess: true, album: saveAlbum })
+  }
+  catch (error) {
+    return res.status(400).send({ sucess: false, msg: error })
+  }
+});
+
+
+//write a get request to get single album information
+//: is used to give parameter like id to route
+router.get("/getOne/:id/", async (req, res) => {
+
+  const filter = { _id: req.params.id }
+
+  const data = await album.findOne(filter)
+  if (data) {
+    return res.status(200).send({ sucess: true, album: data })
+  } else {
+    return res.status(400).send({ sucess: false, msg: "Album not found" })
+  }
 })
+
+//get all the album info
+router.get('/getAll', async (req, res) => {
+
+  const data = await album.find().sort({ createdAt: 1 })
+  if (data) {
+    return res.status(200).send({ sucess: true, album: data })
+  } else {
+    return res.status(400).send({ sucess: false, msg: "Album not found" })
+  }
+})
+
+//updating the album data 
+router.put("/update/:id", async (req, res) => {
+  const filter = { _id: req.params.id }
+  const options = {
+    // upsert creates data if it not exists
+    upsert: true,
+    new: true
+  }
+  try {
+    const result = await album.findOneAndUpdate(filter, {
+      name: req.body.name,
+      imageUrl: req.body.imageUrl,
+    },
+      options)
+
+    return res.status(200).send({ sucess: true, msg: "Album updated sucessfully", data: result })
+
+  }
+  catch (error) {
+    return res.status(400).send({ sucess: false, msg: "Album not found" })
+
+  }
+})
+
+
+//deleting the album in delete route
+router.delete("/delete/:id", async (req, res) => {
+  const filter = { _id: req.params.id }
+  const result = await album.deleteOne(filter)
+
+  if (result) {
+    return res.status(200).send({ sucess: true, msg: "Album deleted sucessfully", data: result })
+  } else {
+    return res.status(400).send({ sucess: false, msg: "Album not found" })
+  }
+})
+
 
 module.exports = router
--- music-player-website/server/routes/songs.js
+++ music-player-website/server/routes/songs.js
@@ -1,7 +1,97 @@
 const router = require("express").Router()
 
-router.get("/getAll", async (req, res) => {
-  return res.json("getting all songs")
+const song = require("../models/song")
+
+//to add a new song
+router.post("/saveNew/", async (req, res) => {
+  const newSong = song(
+    {
+      name: req.body.name,
+      imageUrl: req.body.imageUrl,
+      songUrl: req.body.songUrl,
+      album: req.body.album,
+      artist: req.body.artist,
+      language: req.body.language,
+      category: req.body.category,
+    }
+  );
+
+  try {
+    //save is a method comming from mongoose provided by specific model i.e(song)
+    const saveSong = await newSong.save();
+    return res.status(200).send({ sucess: true, song: saveSong })
+  }
+  catch (error) {
+    return res.status(400).send({ sucess: false, msg: error })
+  }
+});
+
+
+//write a get request to get single song information
+//: is used to give parameter like id to route
+router.get("/getOne/:id/", async (req, res) => {
+
+  const filter = { _id: req.params.id }
+
+  const data = await song.findOne(filter)
+  if (data) {
+    return res.status(200).send({ sucess: true, song: data })
+  } else {
+    return res.status(400).send({ sucess: false, msg: "song not found" })
+  }
+})
+
+//get all the song info
+router.get('/getAll', async (req, res) => {
+
+  const data = await song.find().sort({ createdAt: 1 })
+  if (data) {
+    return res.status(200).send({ sucess: true, song: data })
+  } else {
+    return res.status(400).send({ sucess: false, msg: "song not found" })
+  }
+})
+
+//updating the song data 
+router.put("/update/:id", async (req, res) => {
+  const filter = { _id: req.params.id }
+  const options = {
+    // upsert creates data if it not exists
+    upsert: true,
+    new: true
+  }
+  try {
+    const result = await song.findOneAndUpdate(filter, {
+      name: req.body.name,
+      imageUrl: req.body.imageUrl,
+      songUrl: req.body.songUrl,
+      album: req.body.album,
+      artist: req.body.artist,
+      language: req.body.language,
+      category: req.body.category,
+    },
+      options)
+
+    return res.status(200).send({ sucess: true, msg: "song updated sucessfully", data: result })
+
+  }
+  catch (error) {
+    return res.status(400).send({ sucess: false, msg: "song not found" })
+
+  }
+})
+
+
+//deleting the song in delete route
+router.delete("/delete/:id", async (req, res) => {
+  const filter = { _id: req.params.id }
+  const result = await song.deleteOne(filter)
+
+  if (result) {
+    return res.status(200).send({ sucess: true, msg: "song deleted sucessfully", data: result })
+  } else {
+    return res.status(400).send({ sucess: false, msg: "song not found" })
+  }
 })
 
 module.exports = router

6e16e2e24bc8e6879a86a319d3b01068f71e7a45
Amit-Yadav56 <139137290+Amit-Yadav56@users.noreply.github.com>
2024-04-26 23:18:31 +0530

    Dashboard home section is completed and working in displaying users in the users section

--- music-player-website/client/src/components/DashboardHome.jsx
+++ music-player-website/client/src/components/DashboardHome.jsx
@@ -2,11 +2,16 @@ import React, { useEffect } from 'react'
 import { UseStateValue } from '../context/StateProvider'
 import { getAllUsers, getAllAlbums, getAllSongs, getAllArtists } from '../api'
 import { actionType } from '../context/reducer'
+import { GiLoveSong, GiMusicalNotes } from 'react-icons/gi'
+import { RiUserStarFill } from 'react-icons/ri'
 import { FaUsers, FaMusic, FaFile, FaPersonBooth } from 'react-icons/fa'
+import { bgColor } from '../utils/styles'
 
 export const DashboardCard = ({ icon, name, count }) => {
+
+  const bg_color = bgColor[parseInt(Math.random() * bgColor.length)]
   return (
-    <div className='p-4 w-40 gap-4 h-auto rounded-lg shadow-md bg-teal-500'>
+    <div style={{ background: `${bg_color}` }} className='p-4 w-40 gap-4 h-auto rounded-lg shadow-md flex flex-col items-center '>
       {icon}
       <p className='text-xl text-textColor font-semibold '>{name}</p>
       <p className='text-xl text-textColor font-semibold '>{count}
@@ -55,9 +60,9 @@ const DashboardHome = () => {
   return (
     <div className='w-full p-6 flex items-center justify-evenly flex-wrap'>
       <DashboardCard icon={<FaUsers className="text-2xl  text-textColor " />} name={"Users"} count={allUsers?.length > 0 ? allUsers?.length : 0} />
-      <DashboardCard icon={<FaPersonBooth className="text-2xl  text-textColor " />} name={"Artists"} count={allArtists?.length > 0 ? allArtists?.length : 0} />
-      <DashboardCard icon={<FaFile className="text-2xl  text-textColor " />} name={"Albums"} count={allAlbums?.length > 0 ? allAlbums?.length : 0} />
-      <DashboardCard icon={<FaMusic className="text-2xl  text-textColor " />} name={"Songs"} count={allSongs?.length > 0 ? allSongs?.length : 0} />
+      <DashboardCard icon={<RiUserStarFill className="text-2xl  text-textColor " />} name={"Artists"} count={allArtists?.length > 0 ? allArtists?.length : 0} />
+      <DashboardCard icon={<GiMusicalNotes className="text-2xl  text-textColor " />} name={"Albums"} count={allAlbums?.length > 0 ? allAlbums?.length : 0} />
+      <DashboardCard icon={<GiLoveSong className="text-2xl  text-textColor " />} name={"Songs"} count={allSongs?.length > 0 ? allSongs?.length : 0} />
 
     </div>
   )
--- music-player-website/client/src/components/DashboardUsers.jsx
+++ music-player-website/client/src/components/DashboardUsers.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { UseStateValue } from '../context/StateProvider'
+
 
 const DashboardUsers = () => {
+  const [{ allUsers }, dispatch] = UseStateValue()
   return (
-    <div>DashboardUsers</div>
+
+    <div className='w-full p-4 flex items-center justify-center flex-col'>
+      {/* filters */}
+
+      {/* tabular form data form */}
+      <div className='relative w-full py-12 min-h-[400px] overflow-x-scroll my-4 flex flex-col items-center justify-start p-4 border border-gray-300 rounded-md gap-3 '>
+        {/* Total count of the users */}
+        <div className='absolute top-4 left-4'>
+          <p className='text-xl font-semibold'>
+            Count : <span className='text-sm font-bold text-textColor'>{allUsers?.length}</span>
+          </p>
+
+        </div>
+        {/* table heading */}
+        <div className='w-full min-w-[750px] flex items-center justify-between'>
+          <p className='text-sm text-textColor font-semibold w-275 min-w-[160px] text-center '>Image</p>
+          <p className='text-sm text-textColor font-semibold w-275 min-w-[160px] text-center '>Name</p>
+          <p className='text-sm text-textColor font-semibold w-275 min-w-[160px] text-center '>Email</p>
+          <p className='text-sm text-textColor font-semibold w-275 min-w-[160px] text-center '>Verified</p>
+          <p className='text-sm text-textColor font-semibold w-275 min-w-[160px] text-center '>Created</p>
+          <p className='text-sm text-textColor font-semibold w-275 min-w-[160px] text-center '>Role</p>
+        </div>
+
+        {/* Table Body Content  */}
+      </div>
+    </div>
   )
 }
 

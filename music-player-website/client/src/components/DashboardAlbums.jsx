b6c05f65a4c48acf5e6d0fb216b078c8d82f5533
Amit-Yadav56 <139137290+Amit-Yadav56@users.noreply.github.com>
2024-05-08 23:52:26 +0530

    few changes

--- music-player-website/client/src/components/DashboardAlbums.jsx
+++ music-player-website/client/src/components/DashboardAlbums.jsx
@@ -1,12 +1,12 @@
 
 import React, { useEffect, useState } from "react";
-import { useStateValue } from "../context/StateProvider";
+import { UseStateValue } from "../context/StateProvider";
 
 import { actionType } from "../context/reducer";
 import { getAllAlbums } from "../api";
 
 const DashboardAlbums = () => {
-  const [{ allAlbums }, dispatch] = useStateValue();
+  const [{ allAlbums }, dispatch] = UseStateValue()
   useEffect(() => {
     if (!allAlbums) {
       getAllAlbums().then((data) => {
@@ -17,11 +17,22 @@ const DashboardAlbums = () => {
   return (
     <div className="w-full p-4 flex items-center justify-center flex-col">
       <div className="relative w-full gap-3  my-4 p-4 py-12 border border-gray-300 rounded-md flex flex-wrap justify-evenly">
+        {allAlbums &&
+          allAlbums.map((data, index) => (
+            <>
+              <AlbumCard key={index} data={data} index={index} />
+            </>
+          ))}
 
       </div>
     </div>
   );
 };
+
+
+export const AlbumCard = ({ data, index }) => {
+
+}
 
 
 

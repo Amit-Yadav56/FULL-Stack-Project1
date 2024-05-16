e873ad4c1236bd2d966f3cbdfe9d6d7e395d4995
Amit-Yadav56 <139137290+Amit-Yadav56@users.noreply.github.com>
2024-05-16 12:09:04 +0530

    Count section,adding and searching section created in DashboardAlbum component

--- music-player-website/client/src/components/DashboardArtists.jsx
+++ music-player-website/client/src/components/DashboardArtists.jsx
@@ -4,13 +4,34 @@ import { motion } from "framer-motion";
 
 import { UseStateValue } from "../context/StateProvider";
 import { Link } from "react-router-dom";
-import { IoLogoInstagram, IoLogoTwitter } from "react-icons/io5";
+import { IoLogoInstagram, IoLogoTwitter, IoAdd } from "react-icons/io5";
 import { MdDelete } from "react-icons/md";
 import { deleteArtistById, getAllArtists } from "../api";
 import { actionType } from "../context/reducer";
+import { NavLink } from "react-router-dom";
+import { AiOutlineClear } from "react-icons/ai";
 
 const DashboardArtist = () => {
+
+  const [artistFilter, setArtistFilter] = useState("");
+  const [isFocus, setIsFocus] = useState(false);
+  const [filteredArtists, setFilteredArtists] = useState(null);
+
   const [{ allArtists }, dispatch] = UseStateValue();
+
+
+  //used to filter the songs by search
+  useEffect(() => {
+    if (artistFilter.length > 0) {
+      const filtered = allArtists.filter(
+        (data) =>
+          data.name.toLowerCase().includes(artistFilter)
+      );
+      setFilteredArtists(filtered);
+    } else {
+      setFilteredArtists(null);
+    }
+  }, [artistFilter]);
 
   useEffect(() => {
     if (!allArtists) {
@@ -22,14 +43,69 @@ const DashboardArtist = () => {
 
   return (
     <div className="w-full p-4 flex items-center justify-center flex-col">
-      <div className="relative w-full gap-3  my-4 p-4 py-12 border border-gray-300 rounded-md flex flex-wrap justify-evenly">
-        {allArtists &&
-          allArtists.map((data, index) => (
-            <>
-              <ArtistCard key={index} data={data} index={index} />
-            </>
-          ))}
+      {/*section for adding and searching artists */}
+      <div className="w-full flex justify-center items-center gap-24">
+        <NavLink
+          to={"/dashboard/newData"}
+          className="flex items-center px-4 py-3 border rounded-md border-gray-300 hover:border-gray-400 hover:shadow-md cursor-pointer"
+        >
+          <IoAdd />
+        </NavLink>
+        <input
+          type="text"
+          placeholder="Search here"
+          className={`w-52 px-4 py-2 border ${isFocus ? "border-black shadow-md" : "border-gray-300"
+            } rounded-md bg-transparent outline-none duration-150 transition-all ease-in-out text-base text-textColor font-semibold`}
+          value={artistFilter}
+          onChange={(e) => setArtistFilter(e.target.value)}
+          // set the input box border change to black
+          onBlur={() => setIsFocus(false)}
+          onFocus={() => setIsFocus(true)}
+        />
+
+
+        {/* to erase all the filtered songs */}
+        {artistFilter && (
+          <motion.i
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            whileTap={{ scale: 0.75 }}
+            onClick={() => {
+              setArtistFilter("");
+              setFilteredArtists(null);
+            }}
+          >
+            <AiOutlineClear className="text-3xl text-textColor cursor-pointer" />
+          </motion.i>
+        )}
       </div>
+
+      {/* section to display the filtered songs */}
+      <div className="relative w-full  my-4 p-4 py-12 border border-gray-300 rounded-md">
+        <div className="absolute top-4 left-4">
+          <p className="text-xl font-bold">
+            <span className="text-sm font-semibold text-textColor">
+              Count :{" "}
+            </span>
+            {filteredArtists ? filteredArtists?.length : allArtists?.length}
+          </p>
+        </div>
+
+        <ArtistContainer data={filteredArtists ? filteredArtists : allArtists} />
+      </div>
+
+    </div>
+  );
+};
+
+
+export const ArtistContainer = ({ data }) => {
+  return (
+    <div className=" w-full  flex flex-wrap gap-3  items-center justify-evenly">
+      {data &&
+        data.map((artist, i) => (
+          <ArtistCard key={artist._id} data={artist} index={i} />
+        ))}
     </div>
   );
 };
